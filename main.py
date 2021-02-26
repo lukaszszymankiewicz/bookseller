a7c30d7dbb2453021320a5576261b4989b2f69a8
lukaszszymankiewicz <lukasz_szymankiewicz@op.pl>
2021-02-26 16:33:04 +0100

    prototyle done

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import threading
+
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
+from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
 
 Window.size = (360, 640)
@@ -11,21 +14,37 @@ kv = Builder.load_file("main.kv")
 
 
 class SearchWindow(Screen):
-    def check_book_by_isbn(self, number):
-
-        book = Book(raw_string=number, auto_request=True)
-        import pdb
-
-        pdb.set_trace()
+    pass
 
 
 class ResultsWindow(Screen):
     pass
 
 
+class WaitWindow(Screen):
+    number = StringProperty("")
+
+    def on_enter(self, *args):
+        # this is SO wrong!
+        self.number = self.manager.screens[0].ids["isbn_number"].text
+
+        def callback(data):
+            self.manager.current = "results"
+            self.manager.screens[2].ids["title"].text = str(data["title"])
+            self.manager.screens[2].ids["author"].text = str(data["author"])
+            self.manager.screens[2].ids["avg_prices"].text = str(data["avg_prices"])
+            self.manager.screens[2].ids["sales_number"].text = str(data["sales_number"])
+
+        threading.Thread(target=check_book_by_isbn(self.number, callback)).start()
+
+
+def check_book_by_isbn(number, callback):
+    book = Book(raw_string=number, auto_request=True)
+    callback(book.data)
+
+
 class MyScreenManager(ScreenManager):
-    def __init__(self, **kwargs):
-        super(MyScreenManager, self).__init__(**kwargs)
+    pass
 
 
 class BooksellerApp(App):
