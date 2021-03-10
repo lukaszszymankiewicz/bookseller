3151271ddc6ef0fae7601682c9951f300860d0f6
lukaszszymankiewicz <lukasz_szymankiewicz@op.pl>
2021-03-10 14:52:32 +0100

    query_test_added

---
+++ tests/windows/utils/query_test.py
@@ -0,0 +1,23 @@
+import os
+
+import pytest
+from windows.utils.query import query_book_data
+
+# TODO: move it to some more convinent place
+os.environ["RUN_REQUESTS_AT_TEST"] = "False"
+
+
+@pytest.mark.skipif(
+    os.environ["RUN_REQUESTS_AT_TEST"] == "False", reason="requests test in pipelines only"
+)
+def test_title_is_read_properly(self):
+    # GIVEN
+    code = "978-1-491-91205-8"
+    expected_title = "Python Data Science Handbook: Essential Tools for Working with Data"
+
+    # WHEN
+    results = query_book_data(code)
+    title = results.content["title"]
+
+    # THEN
+    assert expected_title == title
