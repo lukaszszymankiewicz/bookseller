9c4bcee6b7fc6cb4d75016211b7783d6d51243e7
lukaszszymankiewicz <lukasz_szymankiewicz@wmglobal.com>
2021-04-10 21:25:02 +0200

    barcode reader added

--- app/windows/utils/barcode_reader.py
+++ app/windows/utils/barcode_reader.py
@@ -4,7 +4,11 @@ from pyzbar import pyzbar
 def read_barcodes(image):
     barcodes = pyzbar.decode(image)
 
-    if len(barcodes) != 1:
-        raise ValueError
+    if len(barcodes) == 0:
+        return None
 
-    return int(barcodes[0].data)
+    if len(barcodes) > 1:
+        return [int(barcode.data) for barcode in barcodes]
+
+    if len(barcodes) == 1:
+        return int(barcodes[0].data)
--- tests/app/windows/utils/barcode_reader_test.py
+++ tests/app/windows/utils/barcode_reader_test.py
@@ -1,3 +1,4 @@
+import cv2
 import pytest
 from app.windows.utils.barcode_reader import read_barcodes
 
@@ -12,7 +13,7 @@ from app.windows.utils.barcode_reader import read_barcodes
         (5, 9788363944667),
     ],
 )
-def test_reach_barcodes_works_properly(
+def test_reach_barcodes_works_properly_for_prepared_images(
     img_number,
     expected_isbn_number,
     get_sample_image,
@@ -29,3 +30,42 @@ def test_reach_barcodes_works_properly(
 
     # THEN
     assert barcode == expected_isbn_number
+
+
+@pytest.mark.parametrize(
+    "img_number,expected_isbn_number",
+    [
+        (1, 9788386805433),
+        (2, 9788371500480),
+        (3, 9788328365032),
+        (4, 9788379670499),
+        (5, 9788363944667),
+        (6, 9788386805433),
+        (7, 9788371500480),
+        (8, 9788328365032),
+        (9, 9788379670499),
+        (10, 9788363944667),
+        (11, 9788363944667),
+        (12, 9788363944667),
+    ],
+)
+def test_reach_barcodes_works_properly_for_sample_camera_images(
+    img_number,
+    expected_isbn_number,
+    get_sample_camera_image,
+):
+    # GIVEN
+    image_path = get_sample_camera_image(img_number)
+
+    im = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    blur = cv2.GaussianBlur(im, (5, 5), 0)
+    ret, bw_im = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # WHEN
+    barcode = read_barcodes(bw_im)
+
+    # THEN
+    if not barcode:
+        assert False
+    else:
+        assert True
