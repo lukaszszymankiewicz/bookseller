e27893c8e743851d58a0779a6c509bb54b232461
lukaszszymankiewicz <lukasz_szymankiewicz@op.pl>
2021-04-19 09:10:17 +0200

    legal codes added to barcode reader

--- app/windows/utils/barcode_reader.py
+++ app/windows/utils/barcode_reader.py
@@ -1,21 +1,27 @@
 from PIL import Image
 from pyzbar import pyzbar
+from pyzbar.pyzbar import ZBarSymbol
+
+LEGAL_CODE_TYPES = [ZBarSymbol.EAN13, ZBarSymbol.ISBN13]
 
 
 def read_barcodes(image: Image):
-    # TODO: add only EAN-13 code to be read!
     barcodes = pyzbar.decode(image)
 
     if len(barcodes) == 0:
         return None
 
     if len(barcodes) > 1:
-        # TODO: check if any of codes is EAN-13
-
-        import pdb
-
-        pdb.set_trace()
-        return [int(barcode.data) for barcode in barcodes]
+        print(barcodes)
+        for barcode in barcodes:
+            if barcode.type == ZBarSymbol.EAN13:
+                return barcode.data.decode("UTF-8")
+            elif barcode.type == ZBarSymbol.ISBN13:
+                return barcode.data.decode("UTF-8")
+            elif barcode.type == ZBarSymbol.ISBN10:
+                return barcode.data.decode("UTF-8")
+            else:
+                return None
 
     if len(barcodes) == 1:
-        return int(barcodes[0].data)
+        return barcodes[0].data.decode("UTF-8")
